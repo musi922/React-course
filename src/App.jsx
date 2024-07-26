9e02c2bac68fc8e289bc9bdfc99cda1d13b3c264
Richard <richardmusime6@gmail.com>
2024-07-26 09:28:05 +0200

    Added the form for email and password with all of the functionalitiesfor validating working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    email:"", 
+    password:"", 
+    confirmpassword:"",
+    okayToEmail:true
+  })
+  function handleformData(event) {
+    const {name,type,value,checked} = event.target
+    setFormData({
+      ...formData,
+      [name]: type==="checkbox"?checked:value
+    })
+    
+  }
+  
     
     function handleSubmit(event) {
         event.preventDefault()
+        console.log(formData);
+        if (formData.confirmpassword === formData.password) {
+            console.log('Successfully signed up')
+        }
+        else console.log('passwords to not match')
+        if (formData.okayToEmail) {
+            console.log('Thanks for signingup for our newsletter!"');
+            
+        }
     }
     
     return (
@@ -15,31 +39,45 @@ export default function App() {
                     type="email" 
                     placeholder="Email address"
                     className="form--input"
+                    name='email'
+                    value={formData.email}
+                    onChange={handleformData}
                 />
                 <input 
                     type="password" 
                     placeholder="Password"
                     className="form--input"
+                    name='password'
+                    value={formData.password}
+                    onChange={handleformData}
                 />
                 <input 
                     type="password" 
                     placeholder="Confirm password"
                     className="form--input"
+                    name='confirmpassword'
+                    onChange={handleformData}
+                    value={formData.confirmpassword}
                 />
                 
                 <div className="form--marketing">
                     <input
                         id="okayToEmail"
                         type="checkbox"
-                        
+                        name='okayToEmail'
+                        checked={formData.okayToEmail}
+                        onChange={handleformData}
                     />
                     <label htmlFor="okayToEmail">I want to join the newsletter</label>
                 </div>
+                
                 <button 
                     className="form--submit"
+                    onClick={handleSubmit}
                 >
                     Sign up
                 </button>
+
             </form>
         </div>
     )
